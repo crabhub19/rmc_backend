04f1cbf4aafbc440c427591fac6ac38f2053cab3
crabhub19 <3xbishal@gmail.com>
2025-03-04 15:48:35 +0600

    a

--- account/serializers.py
+++ account/serializers.py
@@ -1,8 +1,13 @@
 from djoser.serializers import UserCreateSerializer
+from rest_framework import serializers
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class UserCreateSerializer(UserCreateSerializer):
+    image_url = serializers.SerializerMethodField()
     class Meta(UserCreateSerializer.Meta):
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'password','contract_no', 'image')
+        fields = ('id', 'email', 'first_name', 'last_name', 'password','contract_no','image_url')
+
+    def get_image_url(self, obj):
+        return obj.image.url if obj.image else None
